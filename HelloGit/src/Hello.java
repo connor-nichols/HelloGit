88ca7f3d80a3b650c767184a6bf07fa432b68a97
Connor Nichols <32244381+connor-nichols@users.noreply.github.com>
2021-08-26 16:25:05 -0500

    Add base code to print name

---
+++ HelloGit/src/Hello.java
@@ -0,0 +1,12 @@
+
+/**
+ * Author: Connor Nichols
+ * Date: 08/26/2021
+ *
+*/
+
+public class Hello {
+    public static void main(String args[]) {
+        System.out.println("My name is Connor Nichols!");
+    }
+}

d3483c45fc92dbe6d5b3d3692544eaaadf6d4758
Connor Nichols <32244381+connor-nichols@users.noreply.github.com>
2021-08-26 16:29:07 -0500

    Add base code to print name

--- HelloGit/src/Hello.java
+++ HelloGit/src/Hello.java
@@ -8,5 +8,6 @@
 public class Hello {
     public static void main(String args[]) {
         System.out.println("My name is Connor Nichols!");
+        System.out.println("My major is Computer Science!");
     }
 }

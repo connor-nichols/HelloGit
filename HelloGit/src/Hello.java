6839661794f4d64bcd12fbf972fb271206ec4f5b
BuildTools <unconfigured@null.spigotmc.org>
2021-08-29 13:40:01 -0500

    Added my name and major.

--- HelloGit/src/Hello.java
+++ HelloGit/src/Hello.java
@@ -9,5 +9,7 @@ public class Hello {
     public static void main(String args[]) {
         System.out.println("My name is Connor Nichols!");
         System.out.println("My major is Computer Science!");
+        System.out.println("My name is Elijah Schoneweis!");
+        System.out.println("My major is also Computer Science!");
     }
 }
